7fa6da5b9162f346b1880a9cf3d4ad173063910c
aoiyamada <aoihana1130@gmail.com>
2023-10-12 00:58:36 +0900

    サインアップ機能追加

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,19 @@
 from django.views.generic import TemplateView
 from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import reverse_lazy
+from django.views import generic
+from django.contrib.auth.forms import UserCreationForm
 
 class Index(TemplateView):
     template_name = 'accounts/index.html'
 
 class LoginPage(LoginView):
     template_name = 'accounts/login.html'
+
+class SignInPage(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accounts:profile')
+    template_name = 'accounts/signin.html'
 
 class ProfilePage(TemplateView):
     template_name = 'accounts/profile.html'
